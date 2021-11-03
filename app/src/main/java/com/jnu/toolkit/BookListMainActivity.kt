4e4fb03f8d70e9bfae268592fa18f12aa67ff3a1
Win7GM <Cc741852963@hotmail.com>
2021-11-03 23:16:03 +0800

    updated gradle plugin added context menu for addition and deletion

--- app/src/main/java/com/jnu/toolkit/BookListMainActivity.kt
+++ app/src/main/java/com/jnu/toolkit/BookListMainActivity.kt
@@ -1,17 +1,21 @@
 package com.jnu.toolkit
 
+import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.lang.Exception
+
 
 class BookListMainActivity : AppCompatActivity(){
-    private var bookList:List<Book> = listOf(
+    private var bookList:MutableList<Book> = mutableListOf(
         Book("软件项目管理案例教程（第4版）", R.drawable.book_2),
         Book("创新工程实践", R.drawable.book_no_name),
         Book("信息安全数学基础（第2版）", R.drawable.book_1)
@@ -26,35 +30,116 @@ class BookListMainActivity : AppCompatActivity(){
         container.adapter=BookRecyclerViewAdapter(bookList)
     }
 
-    class BookRecyclerViewAdapter constructor(private val bookList: List<Book>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    fun createMenu(menu: Menu) {
+        val groupID = 0
+        val order = 0
+        val itemID = intArrayOf(1, 2, 3)
+        for (i in itemID.indices) {
+            when (itemID[i]) {
+                1 -> menu.add(groupID, itemID[i], order, "新建")
+                2 -> menu.add(groupID, itemID[i], order, "编辑")
+                3 -> menu.add(groupID, itemID[i], order, "删除")
+            }
+        }
+    }
 
-        class BookViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val position:Int = try {
+            (findViewById<RecyclerView>(R.id.recycle_view_books).adapter as BookRecyclerViewAdapter).mPosition
+        } catch (e: Exception) {
+            Log.d("error",e.localizedMessage, e)
+            return super.onContextItemSelected(item)
+        }
+        when (item.itemId) {
+            1 -> {
+                val dialogueView: View = LayoutInflater.from(this@BookListMainActivity)
+                    .inflate(R.layout.dialogue_input_item, null)
+                val alertDialogBuilder = AlertDialog.Builder(this@BookListMainActivity)
+                alertDialogBuilder.setView(dialogueView)
+
+                alertDialogBuilder.setPositiveButton(
+                    "确定"
+                ) { dialogInterface, i ->
+                    val editName = dialogueView.findViewById<EditText>(R.id.edit_text_name)
+                    bookList.add(
+                        position,
+                        Book(
+                            editName.text.toString(),
+                            R.drawable.book_no_name,
+                        )
+                    )
+
+                    findViewById<RecyclerView>(R.id.recycle_view_books).adapter!!.notifyItemInserted(position)
+                }
+                alertDialogBuilder.setCancelable(false).setNegativeButton(
+                    "取消"
+                ) { dialogInterface, i -> }
+                alertDialogBuilder.create().show()
+            }
+            2 -> {}
+            3 -> {
+                bookList.removeAt(position)
+                findViewById<RecyclerView>(R.id.recycle_view_books).adapter!!.notifyItemRemoved(position)
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
+
+    inner class BookRecyclerViewAdapter constructor(private val bookList: MutableList<Book>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+        internal var mPosition:Int=-1
+
+        private var mContext: Context? = null
+        inner class BookViewHolder(view: View) : RecyclerView.ViewHolder(view),View.OnCreateContextMenuListener{
             val imageView: ImageView
             val textView: TextView
 
             init {
-                // Define click listener for the ViewHolder's View.
                 imageView = view.findViewById(R.id.image_view_book_cover)
                 textView = view.findViewById(R.id.text_view_book_title)
+
+                view.setOnCreateContextMenuListener(this)
             }
+
+            override fun onCreateContextMenu(
+                menu: ContextMenu?,
+                v: View?,
+                menuInfo: ContextMenu.ContextMenuInfo?
+            ) {
+                val mSelectBook:Book = bookList[mPosition]
+                menu!!.setHeaderTitle(mSelectBook.getTitle())
+                (mContext as BookListMainActivity).createMenu(menu)
+            }
+
         }
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            val view = LayoutInflater.from(parent.context)
+            if (mContext == null){
+                mContext= parent.context
+            }
+            val view = LayoutInflater.from(mContext)
                 .inflate(R.layout.item_holder,parent,false)
             return BookViewHolder(view)
         }
 
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
             // 参数是抽象类，要换到实际实现的类
-            val holder:BookViewHolder = holder as BookViewHolder
+            val holder: BookViewHolder = holder as BookViewHolder
             holder.imageView.setImageResource(bookList[position].getCoverResourceId())
             holder.textView.text = bookList[position].getTitle()
+            holder.itemView.setOnLongClickListener {
+                mPosition = holder.layoutPosition
+                false
+            }
         }
 
         override fun getItemCount(): Int {
             return bookList.size
         }
 
+        override fun onViewRecycled(holder: RecyclerView.ViewHolder) {
+            holder.itemView.setOnLongClickListener(null)
+            super.onViewRecycled(holder)
+        }
     }
 
     fun getListBooks():List<Book>{
